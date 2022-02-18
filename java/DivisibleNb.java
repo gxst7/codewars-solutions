d35a24724709345340826b057537ddf65da74693
Alexander Gostilo <alexandergostilo@gmail.com>
2022-02-18 14:57:21 +0300

    add kata solutions

---
+++ java/DivisibleNb.java
@@ -0,0 +1,8 @@
+/**
+ * 8 kyu Is n divisible by x and y?
+ */
+public class DivisibleNb {
+    public static boolean isDivisible(long n, long x, long y) {
+      return n % x == 0 && n % y == 0 ? true : false;
+    }
+}
